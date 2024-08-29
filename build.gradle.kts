b014dba8158d300fa98a5b7d85247a7e6d3054c4
mzntori <mzntori@proton.me>
2024-08-29 22:11:28 +0200

    prepare for publish (in case I ever will lol)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,41 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("org.jetbrains.dokka") version "1.9.20"
     `java-library`
-    id("maven-publish")
-    id("signing")
+    `maven-publish`
+    signing
 }
 
 group = "org.example"
 version = "1.0"
 
-repositories {
-    mavenCentral()
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "io.github.mzntori"
+            artifactId = "twitch-irc-parse"
+            version = "1.0.0"
+
+            from(components["java"])
+
+            pom {
+                name = "Twitch IRC Parser"
+                description = "Parser for Twitch IRC Messages"
+                url = "https://github.com/mzntori/twitch-irc-parser"
+
+                licenses {
+                    license {
+                        name = "The MIT License"
+                        url = "https://opensource.org/license/mit"
+                    }
+                }
+            }
+        }
+    }
 }
 
-publishing {
-    publications.create<MavenPublication>("lib") {
-        
-    }
+repositories {
+    mavenCentral()
 }
 
 dependencies {
@@ -23,10 +43,31 @@ dependencies {
     testImplementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
 kotlin {
     jvmToolchain(17)
+}
+
+java {
+    withSourcesJar()
+}
+
+signing {
+    sign(publishing.publications["maven"])
+}
+
+tasks {
+    dokkaHtml
+    jar {
+        manifest {
+            attributes(
+                mapOf(
+                    "Implementation-Title" to project.name,
+                    "Implementation-Version" to project.version
+                )
+            )
+        }
+    }
+    test {
+        useJUnitPlatform()
+    }
 }
